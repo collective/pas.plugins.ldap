6c00e6715085928af4812d2f595d83dcfbfd75c0
Florian Friesdorf <flo@chaoflow.net>
2010-12-13 19:29:42 +0100

    put back in the im_self catch

--- src/bda/pasldap/utils.py
+++ src/bda/pasldap/utils.py
@@ -38,6 +38,13 @@ class ifnotenabledreturn(object):
         decor.retval = retval
 
     def __call__(decor, method):
+        try:
+            enabled = method.im_self.enabled
+        except AttributeError, e:
+            # XXX: not sure when this happens, but it happens
+            logger.debug('%s' % (str(e),))
+            enabled = False
+
         def wrapper(*args, **kws):
             if not method.im_self.enabled:
                 logger.error('bda ldap disabled: %s defaulting' % \
@@ -49,5 +56,6 @@ class ifnotenabledreturn(object):
                 logger.error('%s' % (str(e),))
                 return decor.retval
             return retval
+
         wrapper.__doc__ = method.__doc__
         return wrapper
