b5861253d32d7fdf69d12c242ccb3435172bd34d
Jens W. Klein <jens@bluedynamics.com>
2011-08-17 13:01:39 +0200

    more testsetup, now plugin installs

--- src/bda/pasldap/setuphandlers.py
+++ src/bda/pasldap/setuphandlers.py
@@ -1,23 +1,17 @@
-print "IMPORT 1" 
 from StringIO import StringIO
 from bda.pasldap._plugin import LDAPPlugin
-print "IMPORT 2" 
 
 
 def isNotThisProfile(context):
     return context.readDataFile("bdapasldap_marker.txt") is None
 
 
-def setupPlugin(context):
-    if isNotThisProfile(context):
-        return 
-    site = context.getSite()
-    pas = site.acl_users
+def _addPlugin(pas):
     installed = pas.objectIds()
     ID = 'ldap_bda'
     TITLE = 'BDA LDAP plugin'
-    if ID not in installed:
-        return TITLE+" already installed."
+    if ID in installed:
+        return TITLE + " already installed."
     plugin = LDAPPlugin(ID, title=TITLE)
     pas._setObject(ID, plugin)
     for info in pas.plugins.listPluginTypeInfo():
@@ -29,3 +23,11 @@ def setupPlugin(context):
             interface,
             [x[0] for x in pas.plugins.listPlugins(interface)[:-1]],
         )
+
+
+def setupPlugin(context):
+    if isNotThisProfile(context):
+        return 
+    site = context.getSite()
+    pas = site.acl_users
+    _addPlugin(pas)
