97f3ca99f455a5a82c59b559336be5785e10dce9
Jens W. Klein <jens@bluedynamics.com>
2011-08-31 11:59:23 +0200

    pep 386 version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.0-beta'
+version = '1.0b1'
 shortdesc ="LDAP Plugin for Zope2 PluggableAuthService (users and groups)"
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'TODO.rst')).read()
