4518b70bfcbdbd675dd773bb6b7b22a62ae4b85d
Jens W. Klein <jens@bluedynamics.com>
2012-04-11 23:07:52 +0200

    hide zope2 profile in quickinstaller

--- src/pas/plugins/ldap/plonecontrolpanel/__init__.py
+++ src/pas/plugins/ldap/plonecontrolpanel/__init__.py
@@ -1,1 +1,11 @@
 import yafowil.loader
+
+from zope.interface import implements
+from Products.CMFQuickInstallerTool.interfaces import INonInstallable
+
+class HiddenProfiles(object):
+    """This hides zope2 profile from the quick installer tool."""
+    implements(INonInstallable)
+
+    def getNonInstallableProducts(self):
+        return ['pas.plugins.ldap']
