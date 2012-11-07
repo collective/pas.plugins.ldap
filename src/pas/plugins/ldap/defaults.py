4f0b318d00cac6ed19b0597ecafd2a9ba13691a6
Robert Niederreiter <office@squarewave.at>
2012-11-07 17:46:13 +0100

    minor formattings

--- src/pas/plugins/ldap/defaults.py
+++ src/pas/plugins/ldap/defaults.py
@@ -35,6 +35,6 @@ DEFAULTS = {
     'groups.queryFilter'      : '(objectClass=groupOfNames)',
     'groups.objectClasses'    : ["groupOfNames"],
     'groups.memberOfSupport'  : False,
-    'groups.expires_attr'      : 'unused',
-    'groups.expires_unit'      : 0,
+    'groups.expires_attr'     : 'unused',
+    'groups.expires_unit'     : 0,
 }
