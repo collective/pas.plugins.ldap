924c053c3828c51d8d88fa5b3800c99c425f2ace
Manuel Reinhardt <reinhardt@syslab.com>
2019-02-08 13:39:31 +0100

    Use own Py23DocChecker to avoid pulling in any extra dependencies.

--- src/pas/plugins/ldap/tests.py
+++ src/pas/plugins/ldap/tests.py
@@ -2,7 +2,6 @@
 from pas.plugins.ldap.testing import PASLDAPLayer
 from plone.testing import layered
 from plone.testing import z2
-from node.ext.ldap.tests import Py23DocChecker
 import doctest
 import pprint
 import unittest
@@ -10,6 +9,21 @@ import unittest
 
 optionflags = doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS
 optionflags = optionflags | doctest.REPORT_ONLY_FIRST_FAILURE
+
+
+class Py23DocChecker(doctest.OutputChecker):
+
+    def check_output(self, want, got, optionflags):
+        if want != got and six.PY2:
+            # if running on py2, ignore any "u" prefixes in the output
+            got = re.sub("(\\W|^)u'(.*?)'", "\\1'\\2'", got)
+            got = re.sub("(\\W|^)u\"(.*?)\"", "\\1\"\\2\"", got)
+            # also ignore "b" prefixes in the expected output
+            want = re.sub("b'(.*?)'", "'\\1'", want)
+            # we get 'ldap.' prefixes on python 3, e.g.
+            # ldap.UNWILLING_TO_PERFORM
+            want = want.lstrip('ldap.')
+        return doctest.OutputChecker.check_output(self, want, got, optionflags)
 
 
 TESTFILES = [
