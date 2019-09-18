399eebca3b370060d64a41e297d974c473e1be4b
Robert Niederreiter <office@squarewave.at>
2019-09-18 17:39:58 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import (
     find_packages,
 )
 
-version = '1.5.4'
+version = '1.5.5.dev0'
 shortdesc = "LDAP/AD Plugin for Plone/Zope PluggableAuthService (users+groups)"
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'TODO.rst')).read()
