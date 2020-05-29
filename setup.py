816ea4be40188d166c86f230910e49e585f5b72f
Philip Bauer <bauer@starzel.de>
2020-05-29 10:20:39 +0200

    Use released version of node.ext.ldap. Require 1.0b12

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         "bda.cache",
         "five.globalrequest",
         "node",
-        "node.ext.ldap>=1.0b9",
+        "node.ext.ldap>=1.0b12",
         "odict",
         "plone.registry",
         "Products.CMFCore",
